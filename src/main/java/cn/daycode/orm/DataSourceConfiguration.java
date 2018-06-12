16690c813a468f030bd4d36b56e7bae70e88bbbf
jl <junjie.liu@haku-mata.cn>
2018-06-13 01:48:36 +0800

    datasource

---
+++ src/main/java/cn/daycode/orm/DataSourceConfiguration.java
@@ -0,0 +1,22 @@
+package cn.daycode.orm;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+   @ConditionalOnBean
+   @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+
+   }
+
+
+}
